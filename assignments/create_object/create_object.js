ec3fed8f8f3749bfc5ac2daab246979c45a66f53
GD26024 <gd26024@gmail.com>
2022-03-08 21:58:47 +0530

    Assignment CreateObject submission

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,13 @@
 function CreateObject(arr) {
     // Write your code here
+    const dum={}
+    for(let i=0;i<arr.length;i++)
+    {
+        dum[arr[i]]=arr[i+1]
+        i=i+1
+
+    }
+    return dum
 }
 
 module.exports = CreateObject;
